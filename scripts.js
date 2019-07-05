31421789529ad1a0bba5abd7778f2f6fc02fd639
Tom <ravi.tom@cheetahdigitl.com>
2019-07-05 14:31:25 +0530

    added more details to person

--- scripts.js
+++ scripts.js
@@ -1,21 +1,23 @@
-class User{
-    constructor(id,a,fname,lname)
-{
-    this.id=id;
-    this.age=a;
-    this.fname=fname;
-    this.lname=lname;
+class User {
+  constructor(id, a, fname, lname) {
+    this.id = id;
+    this.age = a;
+    this.fname = fname;
+    this.lname = lname;
+  }
+
+  printName() {
+    return this.fname + " " + this.lname;
+  }
 }
 
-printName(){
-  return this.fname+' '+this.lname  
+const rt = new User(100, 30, "ravi", "tom");
+
+function printName() {
+  document.querySelector("#Name").innerHTML = rt.printName();
+  document.querySelector(".id").innerHTML = rt.id;
+  document.querySelector(".fname").innerHTML = rt.fname;
+  document.querySelector(".lname").innerHTML = rt.lname;
+  document.querySelector(".age").innerHTML = rt.age;
 }
-
-};
-
-const rt = new User(100,30,'ravi','tom')
-
-function printName(){
-document.querySelector('#Name').innerHTML=rt.printName()
-}
-printName()
+printName();
